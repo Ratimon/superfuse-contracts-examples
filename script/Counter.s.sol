080e5110c9654d77cc935819e7501954d13cef17
Rati Montreewat <ratixoxo@gmail.com>
2024-12-12 19:42:50 +0700

    Build (./): Layouting pkg management && Adding first e2e command

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -7,13 +7,36 @@ import {Counter} from "../src/Counter.sol";
 contract CounterScript is Script {
     Counter public counter;
 
+    string deployConfig;
+
+    constructor() {
+        string memory deployConfigPath = vm.envOr("DEPLOY_CONFIG_PATH", string("/configs/deploy-config.toml"));
+        string memory filePath = string.concat(vm.projectRoot(), deployConfigPath);
+        deployConfig = vm.readFile(filePath);
+    }
+
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        string[] memory chainsToDeployTo = vm.parseTomlStringArray(deployConfig, ".deploy_config.chains");
 
-        counter = new Counter();
 
-        vm.stopBroadcast();
+        
+
+        for (uint256 i = 0; i < chainsToDeployTo.length; i++) {
+            string memory chainToDeployTo = chainsToDeployTo[i];
+
+            console.log("Deploying to chain: ", chainToDeployTo);
+            vm.createSelectFork(chainToDeployTo);
+
+            vm.startBroadcast();
+
+            counter = new Counter();
+
+            vm.stopBroadcast();
+
+        }
+
+       
     }
 }
