20591e15c2c832cb87551df0669ae5a5495ec8e9
Rati Montreewat <ratixoxo@gmail.com>
2024-12-15 11:25:12 +0700

    Test (./test) : Adding ERC20 logic to crosschain logic

--- test/L2NativeSuperchainERC20.t.sol
+++ test/L2NativeSuperchainERC20.t.sol
@@ -16,30 +16,41 @@ import {IERC7802} from "@superfuse-core/interfaces/L2/IERC7802.sol";
 import {ISuperchainERC20} from "@superfuse-core/interfaces/L2/ISuperchainERC20.sol";
 import {L2NativeSuperchainERC20} from "@superfuse-core/L2NativeSuperchainERC20.sol";
 
-// to do: import deploy script
 import {IDeployer, getDeployer} from "@superfuse-deploy/deployer/DeployScript.sol";
 import {DeployL2NativeSuperchainERC20Script} from "@script/DeployL2NativeSuperchainERC20Script.s.sol";
 
 
+// lib for native cross chain 
+import {IOwnable} from "@contracts-bedrock/universal/interfaces/IOwnable.sol";
+
+import {ERC20} from "@solady-v0.0.245/tokens/ERC20.sol";
+import {Ownable} from "@solady-v0.0.245/auth/Ownable.sol";
+
 /// @title SuperchainERC20Test
 /// @notice Contract for testing the SuperchainERC20 contract.
-contract SuperchainERC20Test is Test {
+contract L2NativeSuperchainERC20Test is Test {
 
     string mnemonic = vm.envString("MNEMONIC");
     uint256 ownerPrivateKey = vm.deriveKey(mnemonic, "m/44'/60'/0'/0/", 1); //  address = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8
     address owner = vm.envOr("DEPLOYER", vm.addr(ownerPrivateKey));
+    address alice;
+    address bob;
+
 
     IDeployer deployerProcedue;
-
 
     address internal constant ZERO_ADDRESS = address(0);
     address internal constant SUPERCHAIN_TOKEN_BRIDGE = Predeploys.SUPERCHAIN_TOKEN_BRIDGE;
     address internal constant MESSENGER = Predeploys.L2_TO_L2_CROSS_DOMAIN_MESSENGER;
 
-    SuperchainERC20 public superchainERC20;
+    // SuperchainERC20 public superchainERC20;
+    L2NativeSuperchainERC20 public superchainERC20;
 
     /// @notice Sets up the test suite.
     function setUp() public {
+
+        alice = makeAddr("alice");
+        bob = makeAddr("bob");
 
         deployerProcedue = getDeployer();
         deployerProcedue.setAutoBroadcast(false);
@@ -58,6 +69,8 @@ contract SuperchainERC20Test is Test {
         vm.mockCall(_receiver, _calldata, _returned);
         vm.expectCall(_receiver, _calldata);
     }
+
+    // cross chain logic
 
     /// @notice Tests the `mint` function reverts when the caller is not the bridge.
     function testFuzz_crosschainMint_callerNotBridge_reverts(address _caller, address _to, uint256 _amount) public {
@@ -139,5 +152,136 @@ contract SuperchainERC20Test is Test {
         // Check the total supply and balance of `_from` after the burn were updated correctly
         assertEq(superchainERC20.totalSupply(), _totalSupplyBefore - _amount);
         assertEq(superchainERC20.balanceOf(_from), _fromBalanceBefore - _amount);
+    }
+
+    // main logic
+
+    /// @notice Tests the metadata of the token is set correctly.
+    function testMetadata() public view {
+        assertEq(superchainERC20.name(), "TestToken");
+        assertEq(superchainERC20.symbol(), "TT");
+        assertEq(superchainERC20.decimals(), 18);
+    }
+
+    /// @notice Tests that owner can mint tokens to an address.
+    function testFuzz_mintTo_succeeds(address _to, uint256 _amount) public {
+        vm.expectEmit(true, true, true, true);
+        emit IERC20.Transfer(address(0), _to, _amount);
+
+        vm.prank(owner);
+        superchainERC20.mintTo(_to, _amount);
+
+        assertEq(superchainERC20.totalSupply(), _amount);
+        assertEq(superchainERC20.balanceOf(_to), _amount);
+    }
+
+    /// @notice Tests the mintTo function reverts when the caller is not the owner.
+    function testFuzz_mintTo_succeeds(address _minter, address _to, uint256 _amount) public {
+        vm.assume(_minter != owner);
+
+        // Expect the revert with `Unauthorized` selector
+        vm.expectRevert(Ownable.Unauthorized.selector);
+
+        vm.prank(_minter);
+        superchainERC20.mintTo(_to, _amount);
+    }
+
+    /// @notice Tests that ownership of the token can be renounced.
+    function testRenounceOwnership() public {
+        vm.expectEmit(true, true, true, true);
+        emit IOwnable.OwnershipTransferred(owner, address(0));
+
+        vm.prank(owner);
+        superchainERC20.renounceOwnership();
+        assertEq(superchainERC20.owner(), address(0));
+    }
+
+    /// @notice Tests that ownership of the token can be transferred.
+    function testFuzz_testTransferOwnership(address _newOwner) public {
+        vm.assume(_newOwner != owner);
+        vm.assume(_newOwner != ZERO_ADDRESS);
+
+        vm.expectEmit(true, true, true, true);
+        emit IOwnable.OwnershipTransferred(owner, _newOwner);
+
+        vm.prank(owner);
+        superchainERC20.transferOwnership(_newOwner);
+
+        assertEq(superchainERC20.owner(), _newOwner);
+    }
+
+    /// @notice Tests that tokens can be transferred using the transfer function.
+    function testFuzz_transfer_succeeds(address _sender, uint256 _amount) public {
+        vm.assume(_sender != ZERO_ADDRESS);
+        vm.assume(_sender != bob);
+
+        vm.prank(owner);
+        superchainERC20.mintTo(_sender, _amount);
+
+        vm.expectEmit(true, true, true, true);
+        emit IERC20.Transfer(_sender, bob, _amount);
+
+        vm.prank(_sender);
+        assertTrue(superchainERC20.transfer(bob, _amount));
+        assertEq(superchainERC20.totalSupply(), _amount);
+
+        assertEq(superchainERC20.balanceOf(_sender), 0);
+        assertEq(superchainERC20.balanceOf(bob), _amount);
+    }
+
+    /// @notice Tests that tokens can be transferred using the transferFrom function.
+    function testFuzz_transferFrom_succeeds(address _spender, uint256 _amount) public {
+        vm.assume(_spender != ZERO_ADDRESS);
+        vm.assume(_spender != bob);
+        vm.assume(_spender != alice);
+
+        vm.prank(owner);
+        superchainERC20.mintTo(bob, _amount);
+
+        vm.prank(bob);
+        superchainERC20.approve(_spender, _amount);
+
+        vm.prank(_spender);
+        vm.expectEmit(true, true, true, true);
+        emit IERC20.Transfer(bob, alice, _amount);
+        assertTrue(superchainERC20.transferFrom(bob, alice, _amount));
+
+        assertEq(superchainERC20.balanceOf(bob), 0);
+        assertEq(superchainERC20.balanceOf(alice), _amount);
+    }
+
+    /// @notice tests that an insufficient balance cannot be transferred.
+    function testFuzz_transferInsufficientBalance_reverts(address _to, uint256 _mintAmount, uint256 _sendAmount)
+        public
+    {
+        vm.assume(_mintAmount < type(uint256).max);
+        _sendAmount = bound(_sendAmount, _mintAmount + 1, type(uint256).max);
+
+        vm.prank(owner);
+        superchainERC20.mintTo(address(this), _mintAmount);
+
+        vm.expectRevert(ERC20.InsufficientBalance.selector);
+        superchainERC20.transfer(_to, _sendAmount);
+    }
+
+    /// @notice tests that an insufficient allowance cannot be transferred.
+    function testFuzz_transferFromInsufficientAllowance_reverts(
+        address _to,
+        address _from,
+        uint256 _approval,
+        uint256 _amount
+    ) public {
+        vm.assume(_from != ZERO_ADDRESS);
+        vm.assume(_approval < type(uint256).max);
+        _amount = _bound(_amount, _approval + 1, type(uint256).max);
+
+        vm.prank(owner);
+        superchainERC20.mintTo(_from, _amount);
+
+        vm.prank(_from);
+        superchainERC20.approve(address(this), _approval);
+
+        vm.expectRevert(ERC20.InsufficientAllowance.selector);
+        superchainERC20.transferFrom(_from, _to, _amount);
     }
 }
