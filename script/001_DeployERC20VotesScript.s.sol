f00caded68a710e51839625bfc4b25c956541eaf
Rati Montreewat <ratixoxo@gmail.com>
2025-01-07 20:35:10 +0700

    Build (./): Using correct generated deploy script

--- script/001_DeployERC20VotesScript.s.sol
+++ script/001_DeployERC20VotesScript.s.sol
@@ -1,19 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import {Vm} from "@forge-std-v1.9.1/Vm.sol";
-
-import {DeployScript} from "@superfuse-deploy/deployer/DeployScript.sol";
+pragma solidity ^0.8.25;
 
 import {DefaultDeployerFunction, DeployOptions} from "@superfuse-deploy/deployer/DefaultDeployerFunction.sol";
-
+import {DeployScript} from "@superfuse-deploy/deployer/DeployScript.sol";
 import {MyERC20Votes} from "@main/ERC20Votes.sol";
+import {Vm} from "@forge-std-v1.9.1/Vm.sol";
+
 string constant Artifact_MyERC20Token = "ERC20Votes.sol:MyERC20Votes";
 
+/// @custom:security-contact Consult full internal deploy script at https://github.com/Ratimon/redprint-forge
 contract DeployERC20VotesScript is DeployScript {
-
     MyERC20Votes token;
-
     string name = "TestToken";
     string symbol = "TT";
 
@@ -23,7 +20,6 @@ contract DeployERC20VotesScript is DeployScript {
         DeployOptions memory options = DeployOptions({salt:_salt});
 
         bytes memory args = abi.encode(name, symbol);
-
         return MyERC20Votes(DefaultDeployerFunction.deploy(deployer, "MyERC20Votes", Artifact_MyERC20Token, args, options));
     }
 }
